e59ab91da0eab71e495c1f3f018e54ccc9aaf094
Constantine Kryvomaz <ckryvomaz@gmail.com>
2017-03-29 09:02:44 +0300

    Issue 12 Minor fix.

--- src/sign.rs
+++ src/sign.rs
@@ -82,7 +82,7 @@ impl RlpEncoder {
             }
             len => {
                 let mut len_bytes = vec![];
-                len.to_bytes(&mut len_bytes)
+                len.to_bytes(&mut len_bytes);
                 out.push(0xb7 + len_bytes.len() as u8);
                 out.extend(len_bytes.iter().cloned());
                 out.extend(data.iter().cloned());
