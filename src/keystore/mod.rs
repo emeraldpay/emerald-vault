7a3565d3351029bc92b284c340bed1e3605c5b56
Dmitry Ulanov <dulanov@gmail.com>
2017-04-09 19:17:52 +0200

    `keccak256_mac` field is not well documented
    Expand what MAC acronym really mean, see introduction video
    [TLSnotary details](https://www.youtube.com/watch?v=b4ukd4I8S9A).

--- src/keystore/mod.rs
+++ src/keystore/mod.rs
@@ -50,7 +50,7 @@ pub struct KeyFile {
     /// Key derivation function salt
     pub kdf_salt: [u8; KDF_SALT_BYTES],
 
-    /// Keccak-256 keyed-hash message authentication code (HMAC)
+    /// Keccak-256 based message authentication code
     pub keccak256_mac: [u8; KECCAK256_BYTES],
 
     /// Cipher type
