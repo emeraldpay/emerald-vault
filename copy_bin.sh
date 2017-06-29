4f34fe7d3ad7d1a2465570049849dbf9a50c9c43
r8d8 <ckryvomaz@gmail.com>
2017-06-29 17:10:24 +0300

    Fix for travis build

--- copy_bin.sh
+++ copy_bin.sh
@@ -5,5 +5,5 @@ NAME='$(uname -s)'
 FILENAME='emerald-${TRAVIS_OS_NAME}'
 
 mkdir /tmp/.emerald/
-mv /target/release/emerald /tmp/.emerald/${FILENAME}
+cp ../target/release/emerald /tmp/.emerald/${FILENAME}
 echo "moved release file ${FILENAME}"
