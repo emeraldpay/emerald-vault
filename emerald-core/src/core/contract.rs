7f57a0510ba57f86853efa0d015e56599fc2c37b
Elaine Ou <elaine.ou@gmail.com>
2017-07-08 13:15:21 -0700

    emerald-core/src/core/contract: encode params

--- emerald-core/src/core/contract.rs
+++ emerald-core/src/core/contract.rs
@@ -1,11 +1,9 @@
 //! # Contract
 
-use super::Error;
-use hex::{FromHex, ToHex};
-use std::{fmt, ops};
-use std::str::FromStr;
+use ethabi;
 use ethabi::{Function, Interface};
-use serde_json;
+use ethabi::token::Token;
+use std::fmt;
 
 /// Contract specification
 #[derive(Clone, Debug, Deserialize)]
@@ -16,23 +14,43 @@ pub struct Contract {
 impl Contract {
     /// Try to convert deserialized vector to Contract ABI.
     ///
+    /// # Arguments
+    ///
+    /// * `DATA` - A byte slice
+    ///
     /// # Example
     ///
     /// ```
-    /// let contract = emerald_core::Contract::try_from(&"[]").unwrap();
-    /// assert_eq!(contract.to_string(), "[{},{}]");
+    /// const DATA: &[u8] = b"[{\"constant\":true,\"inputs\":[],\"name\":\"name\",\
+    ///             \"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\
+    ///             \"payable\":false,\"type\":\"function\"}]";
+    /// let contract = emerald_core::Contract::from(DATA).unwrap();
+    /// assert_eq!(contract.to_string(),
+    ///            "Interface([Function(Function { name: \"name\", inputs: [],\
+    ///             outputs: [Param { name: \"\", kind: String }] })])");
     /// ```
-    pub fn try_from(data: &str) -> Result<Self, Error> {
-        let abi: Interface = try!(serde_json::from_str(data)
-            .map_err(|e| Error::InvalidHexLength(e.to_string())));
-        Ok(Contract{ abi: abi })
+    pub fn from(data: &[u8]) -> Result<Self, ethabi::spec::Error> {
+        let abi = Interface::load(data)?;
+        Ok(Contract { abi: abi })
     }
 
-    /// Returns specification of contract function.
+    /// Returns specification of contract function given the function name.
     pub fn function(&self, name: String) -> Option<Function> {
         match self.abi.function(name) {
             Some(f) => Some(Function::new(f)),
-            _ => None
+            _ => None,
+        }
+    }
+
+    /// Encode ABI function call with input params
+    pub fn function_encode(
+        &self,
+        name: String,
+        params: Vec<Token>,
+    ) -> Result<Vec<u8>, ethabi::Error> {
+        match self.function(name) {
+            Some(f) => f.encode_call(params),
+            None => Err(ethabi::Error::InvalidName),
         }
     }
 }
@@ -47,14 +65,40 @@ impl fmt::Display for Contract {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ethabi::spec::{Function as FunctionInterface, Param, ParamType};
 
     #[test]
     fn should_display_contract_abi() {
-        let c = "[{\"constant\":true,\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"type\":\"function\"}]";
-        let contract = Contract(c);
-        assert_eq!(
-            contract.to_string(),
-            c
-        );
+        let c = b"[{\"constant\":true,\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"name\":\"\",\
+                 \"type\":\"string\"}],\"payable\":false,\"type\":\"function\"}]";
+        let contract = Contract::from(c).unwrap();
+        format!("{}", contract);
+    }
+
+    #[test]
+    fn should_return_correct_function() {
+        let c = b"[{\"constant\":true,\"inputs\":[{\"name\":\"\",\"type\":\"address\"}],\"name\":\
+                 \"balanceOf\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":\
+                 false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"name\",\
+                 \"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"type\":\
+                 \"function\"}]";
+        let interface = FunctionInterface {
+            name: "balanceOf".to_owned(),
+            inputs: vec![
+                Param {
+                    name: "".to_owned(),
+                    kind: ParamType::Address,
+                },
+            ],
+            outputs: vec![
+                Param {
+                    name: "a".to_owned(),
+                    kind: ParamType::Uint(256),
+                },
+            ],
+        };
+        let contract = Contract::from(c).unwrap();
+        let f = contract.function("balanceOf".to_string()).unwrap();
+        assert_eq!(f.input_params(), Function::new(interface).input_params());
     }
 }
