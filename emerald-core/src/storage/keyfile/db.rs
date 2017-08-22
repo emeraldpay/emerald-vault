c5410daae01b8a6fc825262edbbc0e1cc9719411
r8d8 <ckryvomaz@gmail.com>
2017-08-22 11:51:27 +0300

    Cleanup

--- emerald-core/src/storage/keyfile/db.rs
+++ emerald-core/src/storage/keyfile/db.rs
@@ -180,7 +180,7 @@ mod tests {
           }
         }"#;
 
-        let (filename, json) = DbStorage::split(&db_item5).unwrap();
+        let (filename, json) = DbStorage::split(&db_item).unwrap();
 
         assert_eq!(
             filename,
