af9d87ab919f91e7abe1e497a0b60ce48ccf73f1
Igor Artamonov <igor@artamonov.ru>
2019-08-13 13:03:41 -0400

    ci 10

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 VERSION_BASE="v$(gitversion /showvariable Major).$(gitversion /showvariable Minor).x"
-CLI_ARCHIVE_NAME="emerald-cli-mac-$APP_VERSION_GIT_TAG"
+CLI_ARCHIVE_NAME="emerald-cli-mac-v$APP_VERSION_GIT_TAG"
 mv target/release/emerald-vault ./emerald
 zip -j "$CLI_ARCHIVE_NAME.zip" emerald
 tar -zcf "$CLI_ARCHIVE_NAME.tar.gz" emerald
