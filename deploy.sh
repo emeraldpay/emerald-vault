c07a91ecf2c9dabdf716fb9b6dfd3353b15476bc
Igor Artamonov <igor@artamonov.ru>
2019-09-02 20:25:43 -0400

    problem: tries to upload master.tar.gz to artifacts

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ tar -zcf "$CLI_ARCHIVE_NAME.tar.gz" target/release/emerald-vault
 echo "Deploy to http://builds.etcdevteam.com/emerald-cli/$VERSION_BASE/"
 
 mkdir deploy
-mv *.zip *.tar.gz deploy/
+mv emerald*.zip emerald*.tar.gz deploy/
 ls -l deploy/
 
 openssl aes-256-cbc -d -in gcloud-travis.json.enc -k $GCP_PASSWD -out gcloud-travis.json
